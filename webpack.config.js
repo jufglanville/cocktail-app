81f23e7db8bfe2679672afd05faecd71bfc600ba
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-23 11:15:03 +0100

    Listen on port 8080 for the devServer.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        })
+  entry: ["babel-polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
-}
+  },
+};
